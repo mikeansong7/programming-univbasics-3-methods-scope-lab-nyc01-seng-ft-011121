601be69967d454440ce57838e7bdafbd92f1854d
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-16 19:35:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "it's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   puts status
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases 
+  puts Mario
+  puts toadstool
+  puts link 
 end
